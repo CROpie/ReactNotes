4be2ed81f1c96ffefa50c05c86fe081662201b54
CROpie <96667010+CROpie@users.noreply.github.com>
2023-11-03 14:56:32 +1100

    changed port

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // export const BaseURL = 'http://127.0.0.1:8000'
-export const BaseURL = 'http://170.64.192.236:90'
+export const BaseURL = 'https://170.64.192.236:443'
 
 export const ContextOptions = [
   { id: 1, title: 'up' },
