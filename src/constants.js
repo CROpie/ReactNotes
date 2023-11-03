fc57f21fc481f520ca5e05ad6f39588d74029386
CROpie <96667010+CROpie@users.noreply.github.com>
2023-11-03 14:38:07 +1100

    ready for deplot

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const BaseURL = 'http://127.0.0.1:8000'
+// export const BaseURL = 'http://127.0.0.1:8000'
+export const BaseURL = 'http://170.64.192.236:90'
 
 export const ContextOptions = [
   { id: 1, title: 'up' },
